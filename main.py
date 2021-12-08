c5a5f66fdf34cd4183e2ad61f3f974e296fd2e91
Anderson Yamamoto <ander182@gmail.com>
2021-12-08 19:45:41 +0000

    first dev commit

--- main.py
+++ main.py
@@ -1,7 +1,16 @@
-from dotenv import load_dotenv
-
-load_dotenv()
-
-print (teste)
+from dotenv import dotenv_values
+import database
+import mqtt
 
 
+
+def main():
+	config = dotenv_values(".env")
+	database = "database.db"
+	
+	mqtt.mqtt_init(config)
+	print (config)
+
+
+if __name__ == '__main__':
+    main()
